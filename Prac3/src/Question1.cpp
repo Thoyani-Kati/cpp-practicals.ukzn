f656ad5d6e38da834bbcf15f72b04ad0340c57bc
Thoyani-Kati <thoyani.tk.kati@Outlook.com>
2026-03-05 20:40:27 +0200

    Add practical 3 question 1 implementation and header

--- Prac3/src/Question1.cpp
+++ Prac3/src/Question1.cpp
@@ -20,21 +20,7 @@ namespace prc3{
         return days;
     }
 
-    bool is_leap_year(long year)
-    {return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));}
 
-    Days days_in_month(int month,long year){
-        switch(month) {
-            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                return 31;
-            case 4: case 6: case 9: case 11:
-                return 30;
-            case 2:
-                return is_leap_year(year) ? 29 : 28;
-            default:
-                return 0;
-        }
-    }
 
     Days get_difference(Date& date1, Date& date2){
         return std::abs(to_days(date1) - to_days(date2));
